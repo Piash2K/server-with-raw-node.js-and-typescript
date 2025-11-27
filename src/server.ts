1d9dae56ff2b19692acfb70fa57782e2f312ff6a
PIASH <zmnobab1999@gmail.com>
2025-11-27 22:10:44 +0600

    get method using custom route handler

--- src/server.ts
+++ src/server.ts
@@ -2,57 +2,83 @@ import http, { IncomingMessage, Server, ServerResponse } from "http";
 import path from "path";
 import config from "./config";
 import { json } from "stream/consumers";
+import addRoutes, { RouteHandler, routes } from "./helpers/RouteHandler";
+addRoutes("GET", "/", (req, res) => {
+  res.writeHead(200, { "content-type": "application/json" });
+  res.end(
+    JSON.stringify({
+      message: "Hello from node js with typescript.......",
+      path: req.url,
+    })
+  );
+});
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("Server is running....");
 
+    const method = req.method?.toUpperCase() || "";
+    const path = req.url || "";
+
+    const methodMap = routes.get(method);
+    const handler: RouteHandler | undefined = methodMap?.get(path);
+    if (handler) {
+      handler(req, res);
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          success: false,
+          message: "Route not found",
+          path,
+        })
+      );
+    }
+    // if (req.url === "/api/users" && req.method === "POST") {
+    // if (req.url === "/api" && req.method === "GET") {
     //route route
-    if (req.url === "/" && req.method === "GET") {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(
-        JSON.stringify({
-          message: "Hello from node js with typescript.......",
-          path: req.url,
-        })
-      );
-    }
+    // if (req.url === "/" && req.method === "GET") {
+    //   res.writeHead(200, { "content-type": "application/json" });
+    //   res.end(
+    //     JSON.stringify({
+    //       message: "Hello from node js with typescript.......",
+    //       path: req.url,
+    //     })
+    //   );
+    // }
     //health route
-    if (req.url === "/api" && req.method === "GET") {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(
-        JSON.stringify({
-          message: "Health status ok",
-          path: req.url,
-        })
-      );
-    }
+    //   res.writeHead(200, { "content-type": "application/json" });
+    //   res.end(
+    //     JSON.stringify({
+    //       message: "Health status ok",
+    //       path: req.url,
+    //     })
+    //   );
+    // }
 
-    if (req.url === "/api/users" && req.method === "POST") {
-      // const user = {
-      //   id: 1,
-      //   name: "alice",
-      // };
-      // res.writeHead(200, { "content-type": "application/json" });
-      // res.end(JSON.stringify(user));
+    // const user = {
+    //   id: 1,
+    //   name: "alice",
+    // };
+    // res.writeHead(200, { "content-type": "application/json" });
+    // res.end(JSON.stringify(user));
 
-      let body = "";
+    // let body = "";
 
-      //listen for data chunk
-      req.on("data", (chunk) => {
-        body += chunk.toString();
-      });
-      req.on("end", () => {
-        try {
-          const parseBody = JSON.parse(body);
-          console.log(parseBody);
-          console.log("Catching current changes")
-          res.end(JSON.stringify(parseBody));
-        } catch (error: any) {
-          console.log(error?.message);
-        }
-      });
-    }
+    // //listen for data chunk
+    // req.on("data", (chunk) => {
+    //   body += chunk.toString();
+    // });
+    // req.on("end", () => {
+    //   try {
+    //     const parseBody = JSON.parse(body);
+    //     console.log(parseBody);
+    //     console.log("Catching current changes");
+    //     res.end(JSON.stringify(parseBody));
+    //   } catch (error: any) {
+    //     console.log(error?.message);
+    //   }
+    // });
   }
 );
 

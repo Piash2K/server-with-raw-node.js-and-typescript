885977a1168ccc4891812dfd560db8bd78f97edf
PIASH <zmnobab1999@gmail.com>
2025-11-28 12:53:40 +0600

    dynamic route handling for PUT Requests

--- src/server.ts
+++ src/server.ts
@@ -2,34 +2,7 @@ import http, { IncomingMessage, Server, ServerResponse } from "http";
 import config from "./config";
 import { RouteHandler, routes } from "./helpers/RouteHandler";
 import "./routes";
-
-function findDynamicRoute(method: string, url: string) {
-  const methodMap = routes.get(method);
-  if (!methodMap) return null;
-  for (const [routePath, handler] of methodMap.entries()) {
-    const routeParts = routePath.split("/");
-    const urlParts = url.split("/");
-
-    if (routeParts.length !== urlParts.length) continue;
-
-    const params: any = {};
-    let matched = true;
-
-    //"/api/users/:id"
-    for (let i = 0; i < routeParts.length; i++) {
-      if (routeParts[i]?.startsWith(":")) {
-        params[routeParts[i]?.substring(1)!] = urlParts[i];
-      } else if (routeParts[i] !== urlParts[i]) {
-        matched = false;
-        break;
-      }
-    }
-    if (matched) {
-      return { handler, params };
-    }
-  }
-  return null;
-}
+import findDynamicRoute from "./helpers/dynamicRouteHandler";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
@@ -42,7 +15,7 @@ const server: Server = http.createServer(
       handler(req, res);
     } else if (findDynamicRoute(method, path)) {
       const match = findDynamicRoute(method, path);
-      (req as any).pause = match?.params;
+      (req as any).params = match?.params;
       match?.handler(req, res);
     } else {
       res.writeHead(404, { "content-type": "application/json" });
